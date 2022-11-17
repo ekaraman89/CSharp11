f58fac9c5ec275d959c8c905355abe442203b809
emrah karaman <ekaraman89@hotmail.com>
2022-11-17 15:05:56 +0300

    Add project files.

---
+++ CSharp11/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSharp11;
+
+Console.WriteLine("Hello, World!");
+
+StudentClass _studentClass = new StudentClass(1, "emrah", "karaman");
+
+StudentRecord _studentRecord = new StudentRecord(1, "emrah", "karaman");
+
+StudentRecord2 _studentRecord2 = new StudentRecord2(1) { FirstName = "emrah", LastName = "karaman" };
+
+Console.WriteLine("Hello, World!");
---
+++ CSharp11/TestClass.cs
@@ -0,0 +1,52 @@
+﻿namespace CSharp11
+{
+    #region class
+
+    public class PersonClass
+    {
+        public string FirstName { get; }
+        public string MiddleName { get; }
+        public string LastName { get; }
+
+        public PersonClass(string firstName, string lastName, string? middleName = null)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            MiddleName = middleName ?? string.Empty;
+        }
+    }
+
+    public class StudentClass : PersonClass
+    {
+        public int ID { get; }
+
+        public StudentClass(int id, string firstName, string lastName, string? middleName = null)
+            : base(firstName, lastName, middleName)
+        {
+            ID = id;
+        }
+    }
+
+    #endregion class
+
+    #region record
+
+    record PersonRecord(string FirstName, string LastName, string MiddleName = "");
+    record StudentRecord(int ID, string FirstName, string LastName, string MiddleName = "") : PersonRecord(FirstName, LastName, MiddleName);
+
+    #endregion record
+
+    #region record v2
+
+    record PersonRecord2()
+    {
+        public required string FirstName { get; init; }
+        public string MiddleName { get; init; }
+        public required string LastName { get; init; }
+    }
+    record StudentRecord2(int ID) : PersonRecord2
+    {
+    }
+
+    #endregion record v2
+}
